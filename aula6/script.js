d4a8701f1cb502ab0b54e7182aa17832bbdb0f63
Bruno Minetti <61476948+BrunoMinetti@users.noreply.github.com>
2020-06-06 19:42:30 -0300

    aula 6

---
+++ aula6/script.js
@@ -0,0 +1,26 @@
+var nome ="Bruno Sanches";
+var idade = 38.5;
+var cursos = ["ADS","SI","BD"];
+var notas = [
+	[7, 6, 5],
+	[3.5, 4, 6],
+	[8, 9, 10]
+];
+
+console.log("Olá "+nome);
+console.log("Idade "+idade+" Anos");
+
+for(var i = 0; i < cursos.length; i++){
+		console.log("Curso: "+cursos[i]);
+		var media = 0;
+		for (var j = 0; j < notas[i].length; j++){
+			media = media + notas[i][j];
+		}
+		media = media / 3;
+		if( media>=6 ){
+			console.log("Aprovado!");
+		}
+		else{
+			console.log("Reprovado!")
+		}
+};
